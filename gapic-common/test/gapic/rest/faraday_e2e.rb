cc9715d6f1c8c2fc5e018d79d46121a03359bf6e
Aza Tulepbergenov <atulep@google.com>
2022-03-24 12:02:33 -0700

    adds faraday streaming test.

---
+++ gapic-common/test/gapic/rest/faraday_e2e.rb
@@ -0,0 +1,63 @@
+
+# frozen_string_literal: true
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "test_helper"
+require "faraday"
+require "pp"
+require 'pry'
+
+##
+# Tests for the REST paged enumerables
+#
+class FaradayE2ETest < Minitest::Test
+  ##
+  # Tests that a `ServerStream` can enumerate all resources via `each`
+  #
+  def test_enumerates_all_resources
+    endpoint = "https://firestore.googleapis.com/v1/projects/its-april/databases/(default)/documents:runQuery"
+
+    conn = Faraday.new url: @endpoint do |conn|
+        conn.headers = { "Content-Type" => "application/json" }
+        conn.request :retry
+        conn.response :raise_error
+        conn.adapter :net_http
+    end
+
+    # A buffer to store the streamed data
+    streamed = []
+
+    res = conn.post(endpoint, "{structuredQuery:{from:{collectionId:'large'}}}") do |req|
+        # Set a callback which will receive tuples of chunk Strings
+        # and the sum of characters received so far
+        req.options.on_data = Proc.new do |chunk, overall_received_bytes|
+            puts "Received #{overall_received_bytes} characters"
+            streamed << chunk
+            #sleep(3)
+            puts chunk
+            #Enumerable.add_chunk(chunk)
+        end
+    end
+    #binding.pry
+    puts "---------"
+    pp res
+    puts "---------"
+    # Joins all response chunks together
+    streamed.join
+  end
+end
+
+
